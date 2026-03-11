7ef201aefd11a5346b4174e25086b91513067de6
Unreallx <artemx2224@yandex@gmail.com>
2026-03-11 22:09:18 +0300

    start the building connection screen

--- lib/screens/connect_screen.dart
+++ lib/screens/connect_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import '../core/app_colors.dart';
 
 class ConnectionScreen extends StatefulWidget {
   const ConnectionScreen({super.key});
@@ -11,24 +10,141 @@ class ConnectionScreen extends StatefulWidget {
 }
 
 class _ConnectionScreenState extends State<ConnectionScreen> {
+  bool bluetoothToogle = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsetsGeometry.only(top: 3, left: 15, right: 10),
+          padding: EdgeInsetsGeometry.only(top: 3, left: 20, right: 20),
           child: SingleChildScrollView(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const Padding(
+                Padding(
                   padding: EdgeInsetsGeometry.only(left: 10),
                   child: Text(
                     "Подключение",
                     style: TextStyle(
-                      fontSize: 40,
-                      color: Colors.white,
+                      fontSize: 28,
+                      color: Theme.of(context).colorScheme.onSurface,
                       fontWeight: FontWeight.w800,
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 10),
+
+                // bluetooth toggle button
+                Container(
+                  width: double.infinity,
+                  height: 140,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(36),
+                    color: Theme.of(context).colorScheme.surfaceContainer,
+                    border: Border.all(
+                      width: 2,
+                      color: Theme.of(context).colorScheme.outlineVariant,
+                    ),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadiusGeometry.circular(36),
+                    child: Material(
+                      color: Colors.transparent,
+                      child: InkWell(
+                        onTap: () => setState(() {
+                          bluetoothToogle = !bluetoothToogle;
+                          // bluetooth conenction callable
+                        }),
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 36, right: 36),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  AnimatedDefaultTextStyle(
+                                    duration: const Duration(milliseconds: 170),
+                                    curve: Curves.easeInOut,
+                                    style: TextStyle(
+                                      fontSize: 19,
+                                      fontWeight: FontWeight.w800,
+                                      color: bluetoothToogle
+                                          ? Theme.of(
+                                              context,
+                                            ).colorScheme.primary
+                                          : Theme.of(
+                                              context,
+                                            ).colorScheme.onSurface,
+                                    ),
+                                    child: const Text("Bluetooth"),
+                                  ),
+                                  Text(
+                                    "Поиск устройств...",
+                                    style: TextStyle(
+                                      color: Theme.of(
+                                        context,
+                                      ).colorScheme.onSurface.withAlpha(150),
+                                      fontWeight: FontWeight.w800,
+                                      fontSize: 10.5,
+                                    ),
+                                  ),
+                                ],
+                              ),
+
+                              //bluetooth switch
+                              AnimatedContainer(
+                                duration: const Duration(milliseconds: 200),
+                                curve: Curves.easeInOut,
+                                width: 80,
+                                height: 45,
+                                decoration: BoxDecoration(
+                                  color: bluetoothToogle
+                                      ? Theme.of(context).colorScheme.primary
+                                      : Theme.of(
+                                          context,
+                                        ).colorScheme.outlineVariant,
+                                  borderRadius: BorderRadius.circular(100),
+                                ),
+                                child: AnimatedAlign(
+                                  duration: const Duration(milliseconds: 200),
+                                  curve: Curves.easeInOut,
+                                  alignment: bluetoothToogle
+                                      ? Alignment.centerRight
+                                      : Alignment.centerLeft,
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                      horizontal: 8,
+                                    ),
+                                    child: AnimatedContainer(
+                                      duration: const Duration(
+                                        milliseconds: 220,
+                                      ),
+                                      curve: Curves.easeInOut,
+                                      width: 30,
+                                      height: 30,
+                                      decoration: BoxDecoration(
+                                        color: bluetoothToogle
+                                            ? Theme.of(
+                                                context,
+                                              ).colorScheme.outline
+                                            : Theme.of(context)
+                                                  .colorScheme
+                                                  .outline
+                                                  .withAlpha(100),
+                                        shape: BoxShape.circle,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
                     ),
                   ),
                 ),
