9bcbeceb19e9cea829dfd0e52587ecdc004b6156
Unreallx <artemx2224@yandex@gmail.com>
2026-03-17 00:08:33 +0300

    add provider support for app

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,17 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'providers/obd_provider.dart';
+import 'providers/bluetooth_provider.dart';
 import 'core/app_themes.dart';
 import 'screens/main_screen.dart';
 
 void main() {
   runApp(
-    ChangeNotifierProvider(
-      create: (context) => ObdProvider(),
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => ObdProvider()),
+        ChangeNotifierProvider(create: (context) => BluetoothProvider()),
+      ],
       child: const RedriveApp(),
     ),
   );
