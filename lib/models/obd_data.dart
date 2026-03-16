861d3d3c58938329fbd68c52b8a8021320f1cc66
Unreallx <artemx2224@yandex@gmail.com>
2026-03-17 00:12:15 +0300

    model of OBD2 Device, add basic support for speed, RPM, Temp, Voltage

--- lib/models/obd_data.dart
+++ lib/models/obd_data.dart
@@ -2,19 +2,26 @@ class ObdData {
   final int rpm;
   final int speed;
   final int engineTemp;
+  final double voltage;
 
-  const ObdData({this.rpm = 0, this.speed = 0, this.engineTemp = 0});
+  const ObdData({
+    this.rpm = 0,
+    this.speed = 0,
+    this.engineTemp = 0,
+    this.voltage = 0.0,
+  });
 
-  ObdData copyWith({int? rpm, int? speed, int? engineTemp}) {
+  ObdData copyWith({int? rpm, int? speed, int? engineTemp, double? voltage}) {
     return ObdData(
       rpm: rpm ?? this.rpm,
       speed: speed ?? this.speed,
       engineTemp: engineTemp ?? this.engineTemp,
+      voltage: voltage ?? this.voltage,
     );
   }
 
   @override
   String toString() {
-    return 'ObdData(rpm: $rpm, speed: $speed km/h, temp: $engineTemp°C)';
+    return 'ObdData(rpm: $rpm, speed: $speed km/h, temp: $engineTemp°C, voltage: $voltage V)';
   }
 }
