aa6297032a66cab439710669f6ab579c3e5f219b
Unreallx <artemx2224@yandex@gmail.com>
2026-03-27 23:37:39 +0300

    update stucture

--- lib/models/obd_device.dart
+++ lib/models/obd_device.dart
@@ -1,12 +1,12 @@
 class ObdDevice {
-  final String name; // Имя, например "OBDII" или "Vgate"
-  final String address; // MAC-адрес (00:11:22:33:44:55)
-  final bool isBle; // Флаг: это новый BLE или старый Classic?
+  final String name;
+  final String address;
+  final bool isBle;
 
   const ObdDevice({
     required this.name,
     required this.address,
-    this.isBle = false, // По умолчанию считаем, что это старый ELM327
+    this.isBle = false,
   });
 
   @override
